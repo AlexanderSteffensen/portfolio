87c03b4ce188564f97cb90d5488e09f6127725f1
AlexanderSteffensen <asteff20@student.aau.dk>
2021-08-01 12:32:31 +0200

    commented gsap out

--- public/main.js
+++ public/main.js
@@ -1,4 +1,3 @@
-//import gsap from 'gsap'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
